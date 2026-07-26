0be7a44f7be7995d2f7dbbcb431e3e9f3e13f980
Thomas Gorgolione <tgorgolione@gmail.com>
2026-07-26 01:20:52 -0400

    Almost ready, just need to test some more

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Kids' Laptops setup
 
-This is how I'm setting up my kids laptop.  Maybe you'll find it useful too.
+This is how I'm setting up my kids' laptops.  Maybe you'll find it useful too.
 
 
 ## Why?
@@ -11,25 +11,32 @@ There's a few alternatives:
 
 - Windows: costs money to upgrade from my old laptops
 
+Instead, I'll be installing Linux.  Fedora Kinoite is the KDE version of Fedora Silverblue, an atomic/immutable version of Fedora.  Immutable means it's really hard to change the core os, and adds some protection against the kids breaking the system (I still remember the time, when I was young, when I thought I'd try formatting the C: drive, it didn't end well).
+
+Additionally, I'll be using pihole to only allow permitted sites (this will be for kids from 6-9ish).  I'll switch to a more permissive solution as my kids get older.  I'm also using `malcontent` to add additional protections against installing flatpaks, and creating my own little "start page" for the kids to see when they open up a browser.
+
+Pull requests for kid sites to be added are most welcome.
+
 
 ## Getting Started
 
-### FEDORA KINOITE for computer setup
-1. Install Fedora Kinote
-2. Setup internet connection
+### Initial setup
+
+1. Install Fedora Kinote (check around the web for instructions - it's pretty much the same as other Linux distros)
+2. Setup an internet connection
 3. Run included setup.sh.  It will:
    - Remove Firefox icon (just a preference)<br>
      https://docs.fedoraproject.org/en-US/atomic-desktops/tips-and-tricks/#_hiding_the_default_browser_firefox
      - Note that this will not remove firefox entry from favorites or taskbar
    - Install Google Chrome<br>
-   `flatpak install com.google.Chrome`
-   - Install malcontent malcontent-control into ostree<br>
-    `sudo rpm-ostree install malcontent malcontent-tools malcontent-control`
+     `flatpak install com.google.Chrome`
+   - Install malcontent (Parental Control) into ostree<br>
+     `sudo rpm-ostree install malcontent malcontent-tools malcontent-control`
    - Place pihole.container in /etc/containers/systemd folder
    - Make /etc/pihole folder
    - Run systemctl daemon-reload
    - Create a podman secret for the password<br>
-   `echo -n "xxxx" | sudo podman secret create pihole_password -`
+     `echo -n "xxxx" | sudo podman secret create pihole_password -`
    - Make system use pihole for DNS
    - Reboot
 
--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import urllib.request
+import json
 from html.parser import HTMLParser
 
 
@@ -24,6 +25,8 @@ class TitleParser(HTMLParser):
 
 htmlTemplate = ""
 renderedSites = ""
+homepageReqHeaders = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36'}
+
 with open("start.template.html", "r") as templateFile:
     htmlTemplate = templateFile.read()
 
@@ -32,27 +35,62 @@ with open("site.template.html", "r") as siteTemplateFile:
 
 with open("whitelist.txt", "w") as finalList:
     with open("kids_allowlist.txt", "r") as kidsList:
-        for domainName in kidsList.readline():
+
+        index = 0
+        for domainName in kidsList.readlines():
+            if domainName.startswith("#"):
+                continue
+
             # get the site icon
+            domainName = domainName.strip()
             remote_url = "https://" + domainName
-            local_filename = domainName + ".png"
-            local_path = "startpage/" + local_filename
+            local_path = "img/" + domainName + ".png"
             site_title = domainName
-            urllib.request.urlretrieve(f"https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url={remote_url}&size=256", local_path)
+
+            print(f"Getting info for {domainName}...")
+
+            try:
+                urllib.request.urlretrieve(f"https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url={remote_url}&size=256", local_path)
+            except Exception as err:
+                #try to save the default icon
+                if hasattr(err, 'code') and (err.code == 404):
+                    with open(local_path, "wb") as missingIconFile:
+                        missingIconFile.write(err.fp.read())
+
+
+            homepageReq = urllib.request.Request(remote_url, headers=homepageReqHeaders)
 
             # look for the title of the homepage
-            with urllib.request.urlopen(remote_url) as response:
-                tmpTitleParser = TitleParser()
-                tmpTitleParser.feed(response.read().decode("utf-8"))
-                site_title = tmpTitleParser.title.strip()
-        
-            renderedSites = renderedSites + siteTemplate.replace("{site_url}", remote_url).replace("{site_img}", local_filename).replace("{site_title}", site_title)
+            try:
+                with urllib.request.urlopen(homepageReq, timeout=10) as response:
+                    tmpTitleParser = TitleParser()
+                    tmpTitleParser.feed(response.read().decode("utf-8", errors='ignore'))
+                    site_title = tmpTitleParser.title.strip().replace('"', '&quot;')
 
-            finalList.writelines(domainName)
+            except Exception as err:
+                pass
+
+            if index % 6 == 0:
+                renderedSites = renderedSites + '<div class="row row-cols-6 row card-row">'
+                
+            renderedSites = renderedSites + siteTemplate.replace("{site_url}", remote_url).replace("{site_img}", local_path).replace("{site_title}", site_title)
+
+            if index % 6 == 5:
+                renderedSites = renderedSites + "</div>"
+                
+            finalList.write(domainName + "\n")
+            index += 1
             
     with open("system_allowlist.txt", "r") as systemList:
         finalList.write(systemList.read())
 
-with open("startpage/index.html", "w") as startFile:
-    completedFile = htmlTemplate.replace("{site_list}", renderedSites)
+unsplash_api = input("Please enter the unsplash API key: ")
+random_image = ""
+
+# look for the title of the homepage
+with urllib.request.urlopen(f"https://api.unsplash.com/photos/random?client_id={unsplash_api}&orientation=landscape&content_filter=high&query=fun%20backgrounds") as response:
+    randomImageResult = json.loads(response.read())
+
+with open("index.html", "w") as startFile:
+    completedFile = htmlTemplate.replace("{site_list}", renderedSites).replace("{random_image}", randomImageResult['urls']['regular']).replace("{artist_name}", randomImageResult['user']['username'])
     startFile.write(completedFile)
