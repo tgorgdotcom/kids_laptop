b36ab953c413db0f2323125987a1dd91e153dfa2
Thomas Gorgolione <tgorgolione@gmail.com>
2026-07-27 20:41:43 -0400

    - added command to add DNS entries to enable google restricted mode - updated allowlist

--- setup2.sh
+++ setup2.sh
@@ -4,3 +4,4 @@ sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comme
 sudo podman exec -it systemd-pihole pihole -g
 sudo podman exec -it systemd-pihole pihole deny --regex '.*'
 sudo pihole-FTL --config dns.cnameRecords '["www.youtube.com,restrict.youtube.com","m.youtube.com,restrict.youtube.com","youtubei.googleapis.com,restrict.youtube.com","youtube.googleapis.com,restrict.youtube.com","www.youtube-nocookie.com,restrict.youtube.com"]'
+sudo pihole-FTL --config dns.cnameRecords '["www.google.com,forcesafesearch.google.com"]'
